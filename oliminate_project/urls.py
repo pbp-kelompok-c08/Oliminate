02f463e49c3d3015cfec7003b979c717d3eebb50
Adjie <adjiesidja20@gmail.com>
2025-10-06 13:39:00 +0700

    Setup project oliminate with 6 feature

---
+++ oliminate_project/urls.py
@@ -0,0 +1,28 @@
+"""
+URL configuration for oliminate_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),               # halaman utama
+    path('users/', include('users.urls')),
+    path('scheduling/', include('scheduling.urls')),
+    path('ticketing/', include('ticketing.urls')),
+    path('reviews/', include('reviews.urls')),
+    path('merchandise/', include('merchandise.urls')),
+]
