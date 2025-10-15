cc0d67c3c07c0049216b922f71bd8c54f90f6cf7
AsukaOurijya <azkaawliya06@gmail.com>
2025-10-15 21:08:26 +0700

    Menambahkan file editprofile.html di folder users/templates

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('edit-profile/', views.edit_profile, name='edit_profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def edit_profile(request):
+    return render(request, 'edit_profile.html')
