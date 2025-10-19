cbb74ad883aec81a667a4f9ebff13728e0d4f36f
AsukaOurijya <azkaawliya06@gmail.com>
2025-10-19 15:49:42 +0700

    menambahkan aplikasi 'users' ke settings.py, menambah isi template halaman main_profile.html, dan menambah isi dan logic masing2 template.

--- users/views.py
+++ users/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import User
+from .forms import UserForm  
 
 def edit_profile(request):
-    return render(request, 'edit_profile.html')
+    user = get_object_or_404(User, id=request.user.id)
+    form = UserForm(request.POST or None, request.FILES or None, instance=user)
+
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('main_profile')  
+
+    context = {
+        'form': form
+    }
+    return render(request, 'edit_profile.html', context)
+
 
 def main_profile(request):
-    return render(request, 'main_profile.html')
+    try:
+        user_obj = request.user.userprofile
+    except Exception:
+        user_obj = User.objects.first()
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, request.FILES, instance=user_obj)
+        if form.is_valid():
+            form.save()
+            return redirect('users:main_profile')
+    else:
+        form = UserForm(instance=user_obj)
+    
+    context = {'form': form, 'user_obj': user_obj}
+
+    return render(request, 'edit_profile.html', context)
