6587b0ac18ed8b9706289b5bec8c78bdaa9fcfa5
AsukaOurijya <azkaawliya06@gmail.com>
2025-10-19 18:56:33 +0700

    perubahan sementara pada urls.py, views.py, edit_profile.html, main_profile.html (errornya blm diresolve)

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
+app_name = 'users'  # supaya bisa pakai {% url 'users:...' %}
+
 urlpatterns = [
-    path('edit-profile/', views.edit_profile, name='edit_profile'),
     path('main-profile/', views.main_profile, name='main_profile'),
+    path('edit-profile/', views.edit_profile, name='edit_profile'),
 ]
+
--- users/views.py
+++ users/views.py
@@ -1,36 +1,32 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from .models import User
-from .forms import UserForm  
 
-def edit_profile(request):
-    user = get_object_or_404(User, id=request.user.id)
-    form = UserForm(request.POST or None, request.FILES or None, instance=user)
-
-    if form.is_valid() and request.method == 'POST':
-        form.save()
-        return redirect('main_profile')  
-
-    context = {
-        'form': form
-    }
-    return render(request, 'edit_profile.html', context)
-
-
+'''
+# Menampilkan profil user yang sedang login
 def main_profile(request):
-    try:
-        user_obj = request.user.userprofile
-    except Exception:
-        user_obj = User.objects.first()
+    user = User.objects.first()
+    return render(request, 'main_profile.html', {'user_obj': user})
+
+# Mengedit profil user yang sedang login
+def edit_profile(request):
+    user = request.user
 
     if request.method == 'POST':
-        form = UserForm(request.POST, request.FILES, instance=user_obj)
-        if form.is_valid():
-            form.save()
-            return redirect('users:main_profile')
-    else:
-        form = UserForm(instance=user_obj)
-    
-    context = {'form': form, 'user_obj': user_obj}
+        user.name = request.POST.get('name')
+        user.email = request.POST.get('email')
+        user.fakultas = request.POST.get('fakultas')
+        if request.FILES.get('foto'):
+            user.foto = request.FILES['foto']
+        user.save()
+        return redirect('users:main_profile')
 
-    return render(request, 'edit_profile.html', context)
+    return render(request, 'edit_profile.html', {'user_obj': user})
+'''
+
+def main_profile(request):
+    user = User.objects.first()
+    return render(request, 'main_profile.html', {'user_obj': user})
+
+def edit_profile(request):
+    user = User.objects.first()
+    return render(request, 'edit_profile.html', {'user_obj': user})
