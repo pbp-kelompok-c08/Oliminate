2374c7ecf22d8da2c014a1b81632b15a7820ad96
AsukaOurijya <azkaawliya06@gmail.com>
2025-10-21 16:00:55 +0700

    Revisi pada forms.py users

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,28 @@ from django import forms
 from .models import User
 
 class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
     class Meta:
         model = User
-        fields = ['name', 'email', 'password', 'foto', 'fakultas']
+        fields = [
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password',      
+            'phone_number',
+            'profile_picture'
+        ]
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password = self.cleaned_data.get("password")
+        
+        if password:
+            user.set_password(password)
+        
+        if commit:
+            user.save()
+            
+        return user
