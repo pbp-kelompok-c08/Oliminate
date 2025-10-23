6312a78db6d8551071c6965e04228e2a9d8ce73f
AsukaOurijya <azkaawliya06@gmail.com>
2025-10-24 00:12:11 +0700

    perubahan pada templates agar terintegrasi dengan users

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
-    path('main_page/', views.main_page, name='main_page'),
-    #path('', views.home, name='home'),
+    path('', views.main_page, name='home'),
 ]
