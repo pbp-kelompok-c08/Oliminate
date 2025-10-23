e437c0aa777c12402680d1dba0fedc2b4ae72c2a
felesiaj <felesiaj26@gmail.com>
2025-10-23 23:46:07 +0700

    benerin conflict

--- main/urls.py
+++ main/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-=======
     path('main_page/', views.main_page, name='main_page'),
->>>>>>> origin/feature/main
     #path('', views.home, name='home'),
 ]
