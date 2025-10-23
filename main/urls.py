d7fcd4e7e87834c9d2f86fa52da1d2fe035caaf7
AsukaOurijya <azkaawliya06@gmail.com>
2025-10-23 14:45:52 +0700

    Menambah page html main page

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('main_page/', views.main_page, name='main_page'),
+    #path('', views.home, name='home'),
 ]
