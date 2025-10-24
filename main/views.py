5af4c29796465a5e864464261ee53fc01c72d910
felesiaj <felesiaj26@gmail.com>
2025-10-24 16:31:30 +0700

    benerin homepage

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from django.http import JsonResponse
 
 def homepage(request):
     # Ambil 5 jadwal terdekat (status 'upcoming')
-    schedules = Schedule.objects.filter(status='upcoming').order_by('date', 'time')[:5]
+    schedules = Schedule.objects.filter(status='upcoming').order_by('date', 'time')[:10]
     return render(request, 'main/homepage.html', {'schedules': schedules})
 
 def get_schedules_json(request):
